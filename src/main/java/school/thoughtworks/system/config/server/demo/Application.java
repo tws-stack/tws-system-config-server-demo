46fc7b5ce492f366dcd398cc46fea37b7b35b790
hhu <hhu@thoughtworks.com>
2017-08-01 11:27:18 +0800

    project init

---
+++ src/main/java/school/thoughtworks/system/config/server/demo/Application.java
@@ -0,0 +1,12 @@
+package school.thoughtworks.system.config.server.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
